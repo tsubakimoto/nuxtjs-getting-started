2f965b92cb56cd446a8a46558cc6d22d0887d8e9
Yuta Matsumura <tkmbmbr@gmail.com>
2024-07-14 15:05:07 +0900

    feat: Add new pages for self-introduction and current time
    The code changes include adding two new pages: `index.vue` for self-introduction and `time.vue` for displaying the current time. These pages are added to the existing `app.vue` file. The `index.vue` page contains a heading and a paragraph for self-introduction, along with a link to the `time.vue` page. The `time.vue` page displays the current time and updates it every second.
    
    Note: The recent user commits and repository commits are not relevant for generating the commit message.

---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <h1>自己紹介</h1>
+        <p>私は、Nuxt.jsの勉強中です。</p>
+
+        <a href="/time">時計を見る</a>
+    </div>
+</template>
---
+++ pages/time.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h1>現在の時刻</h1>
+        <p>{{ currentTime }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            currentTime: new Date().toLocaleTimeString(),
+        };
+    },
+    mounted() {
+        setInterval(() => {
+            this.currentTime = new Date().toLocaleTimeString();
+        }, 1000);
+    },
+};
+</script>
+
+<style scoped>
+h1 {
+    color: #333;
+}
+</style>
